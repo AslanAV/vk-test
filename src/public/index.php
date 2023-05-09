3303c95c2de0ebef1909a604488fd61a0ef2f62e
AslanAV <autlevaslan@yandex.ru>
2023-05-09 18:51:00 +0300

    fix autoload

--- src/public/index.php
+++ src/public/index.php
@@ -9,12 +9,15 @@ use App\Application\Requests\GetEventsRequest;
 use App\Controllers\EventController;
 
 $autoloadPath1 = __DIR__ . '/../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../../vendor/autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+$autoloadPath3 = __DIR__ . '/../../vendor/autoload.php';
 
 if (file_exists($autoloadPath1)) {
     require_once $autoloadPath1;
-} else {
+} elseif (file_exists($autoloadPath2)) {
     require_once $autoloadPath2;
+} else {
+    require_once $autoloadPath3;
 }
 
 $app = new Application();
